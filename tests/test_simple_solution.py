8ffc7f7af062fd7c7beef5cadf91959cf301a7c5
luminita <lumi@sbc.su.se>
2013-09-28 17:16:54 +0200

    Test to check that the solution is correcy

--- tests/test_simple_solution.py
+++ tests/test_simple_solution.py
@@ -5,7 +5,7 @@ import os
 import sys
 import unittest
 sys.path.append(os.path.dirname(__file__).replace("tests", "ala_operator"))
-import naive_solution
+import simple_solution
 import my_exception
 
 def dir_data():
@@ -17,9 +17,9 @@ class TestLoadData(unittest.TestCase):
     def setUp(self):
         unittest.TestCase.setUp(self)
         op_file = os.path.join(dir_data(), "ok_format_operator.txt")
-        self.ns = naive_solution.NaiveSolution(op_file)
+        self.ns = simple_solution.SimpleSolution(op_file)
         self.ns.load_data()
-        self.known_values_op1 = {"1":0.9, "4631":0.15, "46732":1.1}
+        self.known_values_op1 = {"1":0.92, "4631":0.15, "46732":1.1}
         self.known_values_op2 = {"1":0.92, "467":1.0, "48":1.2}
 
     def test_number_operators(self):
@@ -42,6 +42,30 @@ class TestLoadData(unittest.TestCase):
                 self.assertAlmostEqual(value, hash_table[prefix])
         
         
+class TestGetSolution(unittest.TestCase):
+    """ Test that the solution returned is correct """
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        op_file = os.path.join(dir_data(), "ok_format_operator.txt")
+        self.ns = simple_solution.SimpleSolution(op_file)
+        self.ns.load_data()
+        self.known_solutions = {"112345":[("A","1",0.92),("B","1",0.92)], \
+            "99999":[], "467":[("A","46",0.17)], "486536":[("B","48",1.2)], \
+            "463127":[("A","4631", 0.15)], "46732125":[("B","467",1.0)]}
+
+    def test_known_values(self):
+        """ The functions returns the correct solution for a few examples """
+        for phone_number, correct_sol in self.known_solutions.iteritems():
+            sol = self.ns.get_solution(phone_number)
+            print phone_number, sol, correct_sol
+            self.assertEqual(len(correct_sol), len(sol))
+            i = 0
+            while i<len(correct_sol):
+                self.assertEqual(correct_sol[i][0], sol[i][0])
+                self.assertEqual(correct_sol[i][1], sol[i][1])                    
+                self.assertAlmostEqual(correct_sol[i][2], sol[i][2])
+                i += 1
+
 
 if __name__ == '__main__':
     unittest.main()
