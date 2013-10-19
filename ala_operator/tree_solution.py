e418d70062563948501ee0d8dac18ac6ffd47727
luminita <lumi@sbc.su.se>
2013-10-19 15:55:50 +0200

    Implemented load and get solution functions

--- ala_operator/tree_solution.py
+++ ala_operator/tree_solution.py
@@ -1,3 +1,7 @@
+import my_exception
+import sys
+
+
 class PrefixTree:
     def __init__(self):
         """ children is a dictionary character -> Trie  """
@@ -42,23 +46,64 @@ def search(curr_node, number, price, path):
     path.append(st)
     return search(cn, number[1:], price, path)
         
+class TreeSolution:
+    def __init__(self):
+        self.trees = []
+        self.ids = [] 
 
+    def add(self, tree, tree_id):
+        self.trees.append(tree)
+        self.ids.append(tree_id)
 
-          
+    def get_solution(self, phone_number):
+        """ Get the best solution by searching in all trees """
+        solution = [(None, None, 1.0e6)]
+        for (tree, tree_id) in zip(self.trees, self.ids):
+            price, path =  search(tree, phone_number, None, [])
+            if price <= solution[0][2]:
+                solution[0] = (tree_id, "".join(path), price)
+
+        return solution 
+
+    def load_data(self, operator_file):
+        """ Load the data of each operator """
+        try:
+            in_handler = open(operator_file)
+            line = in_handler.readline()
+            while line:
+                # load the offers of each operator
+                if line.startswith("Operator"):
+                    tree = PrefixTree()
+                    operator_id = line.split()[1].strip()
+                    line = in_handler.readline()
+                    while line and not line.startswith("Operator"):
+                        prefix = line.split()[0].strip()
+                        price = float(line.split()[1])
+                        tree.add(prefix, price)
+                        line = in_handler.readline()
+                    self.trees.append(tree)
+                    self.ids.append(operator_id)
+                else:
+                    # I just raise an exception if the file includes anything else than expected
+                    raise my_exception.MyException("Incorrect operator file format") 
+            in_handler.close()
+        except Exception, e:
+            sys.exit(str(e))
+            
+    def print2screen(self):
+      for (tree, tree_id) in zip(self.trees, self.ids):
+          print tree_id
+          tree.visit()
+          print "///////////////////"
+
+    
 def main():
-    t = PrefixTree()
-    t.add("1", 0.9)
-    t.add("268", 5.1)
-    t.add("4631", 0.15)
-    t.add("46", 0.17)
-    t.add("4673", 0.9)
-    t.add("46732", 1.1)
-    t.add("468", 0.15)
-    t.add("4620", 0.0)
-    t.visit()
-    print "---------"
-    price, path =  search(t, "4667453226", None, [])
-    print price, path
+    t = TreeSolution()
+    t.load_data("../examples/ex_operators_1.txt")
+    t.print2screen()
+    print t.get_solution("4672974752876")
+
+
   
 if __name__ == '__main__':
     main()     
